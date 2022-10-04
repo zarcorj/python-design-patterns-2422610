bec119ee18dac542573dcf7bbd50c18984f832d6
Javier Zarco Rebollar <javier.zarco@ab-inbev.com>
2022-10-03 21:59:23 -0500

    Implement Singleton design pattern without invoking parent constructor

--- singleton.py
+++ singleton.py
@@ -1,12 +1,33 @@
-class Borg:
-    
-    """The Borg design pattern"""
-    
+from tokenize import Single
 
-        
+
+class Borg:
+
+    """The Borg design pattern"""
+
+    _shared_data = {} #Attribute dictionary
+
+    def __init__(self):
+        self.__dict__ = self._shared_data #Make an attribute dictionary
+
 
 class Singleton(Borg): 
-    
-    """The singleton class"""
-   
 
+    """The singleton class"""
+    def __init__(self, **kwards):
+        #Borg.__init__(self)
+        #super().__init__()
+        self._shared_data.update(kwards) #Update the attribute dictionary
+
+    def __str__(self):
+        return str(self._shared_data)
+
+# Create a singleton object
+x = Singleton(HTTP = "Hyper Text Transfer Protocol")
+print(x)
+
+# Create another singleton object
+y = Singleton(SNMP = "Simple Network Management Protocol")
+print(y)
+
+print(y.__dict__)
