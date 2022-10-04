96f590ccbfb1f5858a0e42f0b973310fcd1ce460
Javier Zarco Rebollar <javier.zarco@ab-inbev.com>
2022-10-03 21:37:44 -0500

    Implement Factory design pattern

--- factory.py
+++ factory.py
@@ -1,9 +1,32 @@
 class Dog:
 
-	"""A simple dog class"""
+    """A simple dog class"""
 
-	def __init__(self, name):
-		self._name = name
+    def __init__(self, name):
+        self._name = name
 
-	def speak(self):
-		return "Woof!"
+    def speak(self):
+        return "Woof!"
+
+
+class Cat:
+
+    """A simple dog class"""
+
+    def __init__(self, name):
+        self._name = name
+
+    def speak(self):
+        return "Meow!"
+    
+def get_pet(pet="cat"):
+    """The Factory Method"""
+
+    pets = dict(dog=Dog("Hope"), cat=Cat("Felix"))
+    return pets[pet]
+
+d = get_pet("dog")
+print(d.speak())
+
+c = get_pet("cat")
+print(c.speak())
