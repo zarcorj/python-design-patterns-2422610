000aff9e03140fa8f793435748012b2a36326780
Javier Zarco Rebollar <javier.zarco@ab-inbev.com>
2022-10-13 11:53:37 -0500

    Implement Adapter design pattern

--- adapter.py
+++ adapter.py
@@ -1,47 +1,52 @@
 class Korean:
-	"""Korean speaker"""
-	def __init__(self):
-		self.name = "Korean"
+    """Korean speaker"""
 
-	def speak_korean(self):
-		return "An-neyong?"
+    def __init__(self):
+        self.name = "Korean"
+
+    def speak_korean(self):
+        return "An-neyong?"
+
 
 class British:
-	"""English speaker"""
-	def __init__(self):
-			
+    """English speaker"""
 
-	#Note the different method name here!
-	def speak_english(self):
-			
+    def __init__(self):
+        self.name = "English"
+
+    # Note the different method name here!
+    def speak_english(self):
+        return "Hello!"
+
 
 class Adapter:
-	"""This changes the generic method name to individualized method names"""
+    """This changes the generic method name to individualized method names"""
 
-	def __init__(self, object, **adapted_method):
-		"""Change the name of the method"""
-		self._object
+    def __init__(self, object, **adapted_method):
+        """Change the name of the method"""
+        self._object = object
 
-		#Add a new dictionary item that establishes the mapping between the generic method name: speak() and the concrete method
-		#For example, speak() will be translated to speak_korean() if the mapping says so
-		
+        # Add a new dictionary item that establishes the mapping between the generic method name: speak() and the concrete method
+        # For example, speak() will be translated to speak_korean() if the mapping says so
+        self.__dict__.update(adapted_method)
 
-	def __getattr__(self, attr):
-		"""Simply return the rest of attributes!"""
-		
-		
-#List to store speaker objects
+    def __getattr__(self, attr):
+        """Simply return the rest of attributes!"""
+        return getattr(self._object, attr)
+
+
+# List to store speaker objects
 objects = []
 
-#Create a Korean object
+# Create a Korean object
+korean = Korean()
 
+# Create a British object
+british = British()
 
-#Create a British object
-
-
-#Append the objects to the objects list
-
+# Append the objects to the objects list
+objects.append(Adapter(korean, speak=korean.speak_korean))
+objects.append(Adapter(british, speak=british.speak_english))
 
 for obj in objects:
-	print("{} says '{}'\n".format(obj.name, obj.speak()))
-
+    print("{} says '{}'\n".format(obj.name, obj.speak()))
